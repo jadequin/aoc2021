a9848541a9dda46af3dc8685a3fb752ecf710aba
Till Nitsche <till.nitsche@web.de>
2021-12-06 01:15:41 +0100

    fin day4

--- days/d04/Day4.java
+++ days/d04/Day4.java
@@ -8,7 +8,7 @@ import java.util.stream.Collectors;
 import interfaces.Day;
 
 public class Day4 extends Day {
-
+  
   public Day4() {
     super(4);
   }
@@ -18,7 +18,7 @@ public class Day4 extends Day {
     // TODO Auto-generated method stub
     return null;
   }
-
+  
   @Override
   public String resultLivPart2() {
     // TODO Auto-generated method stub
@@ -27,78 +27,177 @@ public class Day4 extends Day {
 
   @Override
   public String resultTillPart1() {
-    String input = fileReaderTill.getString();
+    class Board {
+      int[][] field;
+    
+      Board(String strBoard) {
+        this.field = parseBoard(strBoard);
+      }
+    
+      private int[][] parseBoard(String strBoard) {
+        int[][] res = new int[5][5];
+    
+        String[] rows = strBoard.trim().split("\r\n");
+        for (int i = 0; i < res.length; i++)
+          res[i] = Arrays.stream(rows[i].trim().split("\\s+"))
+            .mapToInt(Integer::parseInt)
+            .toArray();
+        
+        return res;
+      }
 
-    var numbers = Arrays.asList(input.substring(0, input.indexOf("\r\n")).split(","))
-      .stream()
-      .map(Integer::parseInt)
-      .collect(Collectors.toList());
+      @Override
+      public String toString() {
+        var sb = new StringBuilder();
+        for(int i = 0; i < field.length; i++) {
+          for(int j = 0; j < field[i].length; j++)
+            sb.append(String.format("%3d", field[i][j]));
+          sb.append("\n");
+        }
+        return sb.toString();
+      }
 
-      List<ArrayList<Integer>> boards = Arrays.asList(input.substring(input.indexOf("\r\n\r\n")).trim().split("\r\n\r\n"))
-        .stream()
-        .map(b -> {
-          return new ArrayList<Integer>(
-            Arrays.asList(b.trim().split("\\s+"))
-              .stream()
-              .map(Integer::parseInt)
-              .collect(Collectors.toList())
-          );
-        })
-        .collect(Collectors.toList());
-
-      var bingoRows = List.of(
-        List.of(0,1,2,3,4),
-        List.of(5,6,7,8,9),
-        List.of(10,11,12,13),
-        List.of(15,16,17,18,19),
-        List.of(20,21,22,23,24),
-        List.of(0,5,10,15,20),
-        List.of(1,6,11,16,21),
-        List.of(2,7,12,17,22),
-        List.of(3,8,13,18,23),
-        List.of(4,9,14,19,24)
-      );
-      
-      for (var n : numbers) {
-        for (var b : boards) {
-          var hit = b.stream()
-            .filter(x -> x == n)
-            .findFirst()
-            .orElse(null);
-
-          if (hit != null) {
-            int potentialBingoFinisher = hit;
-            hit = -1;
-
-            // check if there is a bingo
-            boolean bingo = true;
-            for (int i = 0; i < bingoRows.size(); i++) {
-              for (var num : bingoRows.get(i)) {
-                if (b.get(num) != -1) {
-                  bingo = false;
-                  break;
-                }
-              }
-              // Big Hit
-              if (bingo) {
-                int res = b.stream()
-                  .mapToInt(Integer::intValue)
-                  .sum() + 5;
-                
-                return "" + res * potentialBingoFinisher;
-              }
+      Integer shot(int num) {
+        boolean hit = false;
+        int finY = -1, finX = -1;
+        for (int y = 0; y < field.length; y++) {
+          for (int x = 0; x < field[y].length; x++) {
+            if (field[y][x] == num) {
+              field[y][x] = -1;
+              finY = y;
+              finX = x;
+              hit = true;
             }
           }
         }
+
+        if (hit) {
+          int sumY = 0, sumX = 0;
+          for (int i = 0; i < field.length; i++) {
+            sumY += field[finY][i];
+            sumX += field[i][finX];
+          }
+          if (sumY == -5 || sumX == -5) {
+            int res = 0;
+            for (int i = 0; i < field.length; i++)
+              for(int j = 0; j < field[i].length; j++)
+                res += field[i][j] == -1 ? 0 : field[i][j];
+
+            return res * num;
+          }
+        }
+        return null;
       }
-      
+    }
+
+    String input = fileReaderTill.getString();
+    
+    int[] numbers = Arrays.stream(input.substring(0, input.indexOf("\r\n")).trim().split(","))
+    .mapToInt(Integer::parseInt)
+    .toArray();
+    
+    Board[] boards = Arrays.stream(input.substring(input.indexOf("\r\n\r\n")).trim().split("\r\n\r\n"))
+    .map(x -> new Board(x))
+    .toArray(Board[]::new);
+    
+    for (var n : numbers)
+      for (var b : boards) {
+        Integer s = b.shot(n);
+        if (s != null)
+          return "" + s;
+      }
+
     return null;
   }
 
   @Override
   public String resultTillPart2() {
-    // TODO Auto-generated method stub
-    return null;
+    class Board {
+      boolean bingo = false;
+      int[][] field;
+    
+      Board(String strBoard) {
+        this.field = parseBoard(strBoard);
+      }
+    
+      private int[][] parseBoard(String strBoard) {
+        int[][] res = new int[5][5];
+    
+        String[] rows = strBoard.trim().split("\r\n");
+        for (int i = 0; i < res.length; i++)
+          res[i] = Arrays.stream(rows[i].trim().split("\\s+"))
+            .mapToInt(Integer::parseInt)
+            .toArray();
+        
+        return res;
+      }
+
+      @Override
+      public String toString() {
+        var sb = new StringBuilder();
+        for(int i = 0; i < field.length; i++) {
+          for(int j = 0; j < field[i].length; j++)
+            sb.append(String.format("%3d", field[i][j]));
+          sb.append("\n");
+        }
+        return sb.toString();
+      }
+
+      Integer shot(int num) {
+        boolean hit = false;
+        int finY = -1, finX = -1;
+        for (int y = 0; y < field.length; y++) {
+          for (int x = 0; x < field[y].length; x++) {
+            if (field[y][x] == num) {
+              field[y][x] = -1;
+              finY = y;
+              finX = x;
+              hit = true;
+            }
+          }
+        }
+
+        if (hit) {
+          int sumY = 0, sumX = 0;
+          for (int i = 0; i < field.length; i++) {
+            sumY += field[finY][i];
+            sumX += field[i][finX];
+          }
+          if (sumY == -5 || sumX == -5) {
+            int res = 0;
+            for (int i = 0; i < field.length; i++)
+              for(int j = 0; j < field[i].length; j++)
+                res += field[i][j] == -1 ? 0 : field[i][j];
+
+            bingo = true;
+            return res * num;
+          }
+        }
+        return null;
+      }
+    }
+
+    String input = fileReaderTill.getString();
+    
+    int[] numbers = Arrays.stream(input.substring(0, input.indexOf("\r\n")).trim().split(","))
+    .mapToInt(Integer::parseInt)
+    .toArray();
+    
+    Board[] boards = Arrays.stream(input.substring(input.indexOf("\r\n\r\n")).trim().split("\r\n\r\n"))
+    .map(x -> new Board(x))
+    .toArray(Board[]::new);
+    
+    Integer res = null;
+    for (var n : numbers)
+      for (var b : boards) {
+        if (!b.bingo) {
+          Integer s = b.shot(n);
+          if (s != null)
+            res = s;
+        }
+      }
+
+    return "" + res;
   }
 
 }
